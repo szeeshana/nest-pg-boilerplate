ae374cd6932991bc32ea5800dd1aa838011742d1
zeeshan <zeeshan.altaf27@gmail.com>
2023-04-19 15:24:35 +0500

    add seeder

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -7,24 +7,30 @@ import {
   BaseEntity,
 } from 'typeorm';
 import { UserType } from '../enum/user.enum';
+import { Factory } from 'nestjs-seeder';
 
 @Entity('user')
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Factory('Zeeshan')
   @Column()
   firstName: string;
 
+  @Factory('Altaf')
   @Column()
   lastName: string;
 
+  @Factory('test@gmail.com')
   @Column()
   email: string;
 
+  @Factory('test123')
   @Column({ select: false })
   password: string;
 
+  @Factory('USER')
   @Column({
     type: 'enum',
     enum: UserType,
